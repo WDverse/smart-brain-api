0f21bd7cbe479defadd8e5a707a2dc47fefa4587
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-12-10 16:40:28 -0500

    created file for register endpoint

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import knex from "knex";
 import bcrypt from "bcrypt-nodejs";
+import register from "./controllers/register.js";
 
 const db = knex({
   client: "pg",
@@ -21,31 +22,7 @@ app.use(express.json());
 app.use(cors());
 
 app.post("/register", (req, res) => {
-  const { name, email, password } = req.body;
-  const hash = bcrypt.hashSync(password);
-  db.transaction((trx) => {
-    trx
-      .insert({
-        hash,
-        email,
-      })
-      .into("login")
-      .returning("email")
-      .then((loginEmail) => {
-        return trx("users")
-          .returning("*")
-          .insert({
-            name,
-            email: loginEmail[0].email,
-            joined: new Date(),
-          })
-          .then((user) => {
-            res.json(user[0]);
-          });
-      })
-      .then(trx.commit)
-      .catch(trx.rollback);
-  }).catch((err) => res.status(400).json("Unable to register"));
+  register (req, res, db, bcrypt);
 });
 
 app.post("/signin", (req, res) => {
@@ -75,31 +52,31 @@ app.post("/signin", (req, res) => {
 });
 
 app.get("/profile/:id", (req, res) => {
-    const { id } = req.params;
-    db.select("*")
-      .from("users")
-      .where({ id })
-      .then((user) => {
-        if (user.length) {
-          res.json(user[0]);
-        } else {
-          res.status(404).json("No user found");
-        }
-      })
-      .catch((err) => res.json("error getting user"));
-  });
+  const { id } = req.params;
+  db.select("*")
+    .from("users")
+    .where({ id })
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(404).json("No user found");
+      }
+    })
+    .catch((err) => res.json("error getting user"));
+});
 
-  app.put("/image", (req, res) => {
-    const { id } = req.body;
-    db("users")
-      .where("id", "=", id)
-      .increment("entries", 1)
-      .returning("entries")
-      .then((entries) => {
-        res.json(entries[0].entries);
-      })
-      .catch((err) => res.json("unable to get entries"));
-  });
+app.put("/image", (req, res) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0].entries);
+    })
+    .catch((err) => res.json("unable to get entries"));
+});
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
