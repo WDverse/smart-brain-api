ac8c0aec49ee8026272dae96ac13d9a5373cc5e3
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-12-09 23:47:55 -0500

    created endpoint to update entries when user submits image URL

--- server.js
+++ server.js
@@ -88,7 +88,18 @@ app.get("/profile/:id", (req, res) => {
       })
       .catch((err) => res.json("error getting user"));
   });
-  
+
+  app.put("/image", (req, res) => {
+    const { id } = req.body;
+    db("users")
+      .where("id", "=", id)
+      .increment("entries", 1)
+      .returning("entries")
+      .then((entries) => {
+        res.json(entries[0].entries);
+      })
+      .catch((err) => res.json("unable to get entries"));
+  });
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
